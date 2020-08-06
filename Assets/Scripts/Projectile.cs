159fc22611e39fdf9ed8510d6568629962242933
CupcakesLords <42338474+CupcakesLords@users.noreply.github.com>
2020-08-06 17:42:09 +0700

    initial commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+            e.Fix();
+        
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (transform.position.magnitude > 300.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
